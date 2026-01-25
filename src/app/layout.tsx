a1cd57a3e3c3d8178b6bb771398ac75719c08a70
Michael J Salgado <92074543+earthhworms@users.noreply.github.com>
2026-01-25 10:36:08 -0500

    Added components, adjusted css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Bunny on a Bender | Handmade & Upcycled Rave Gear",
   description: "null",
 };
+
+const openSans = localFont({
+  src: "../app/fonts/OpenSans.ttf",
+  variable: "--open-sans",
+  style: "normal",
+  weight: "600",
+  display: "swap",
+});
+
+const specialGothic = localFont ({
+  src: "../app/fonts/SpecialGothic.ttf",
+  variable: "--special-gothic",
+  style: "normal",
+  display: "swap"
+})
 
 export default function RootLayout({
   children,
@@ -24,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${openSans.variable} ${specialGothic.variable}`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div>
-          <h1>hi gf</h1>
+        <div className={styles.title}>
+          <h1>HANDMADE RAVE GEAR</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores a nesciunt fugit doloribus provident beatae iusto assumenda facere, id perferendis minus, laborum consectetur iste error maxime suscipit quis voluptatibus nam!</p>
         </div>
       </main>
     </div>
