ba3e5609cac804e87c69a1636b4b8361b0ca9c4f
Michael J Salgado <92074543+earthhworms@users.noreply.github.com>
2026-03-12 13:18:58 -0400

    gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.title}>
           <h1 className={styles.rainbowText}>HAND <br></br>MADE <br></br>RAVE<br></br>GEAR</h1>
-          <h2 className={styles.rainbowText}>FOR RAVERS, <br></br>BY A RAVER</h2>
+          <h2>FOR RAVERS, <br></br>BY A RAVER</h2>
           <a className={styles.landingButton}>SHOP ITEMS</a>
         </div>
 
