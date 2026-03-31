e10141103631fb06a64a8047a9a665dd66f8734e
Michael J Salgado <92074543+earthhworms@users.noreply.github.com>
2026-03-31 04:12:51 -0400

    cart drawer update

--- src/components/CartDrawer/CartDrawer.tsx
+++ src/components/CartDrawer/CartDrawer.tsx
@@ -197,7 +197,7 @@ export default function CartDrawer() {
                       onClick={() => handleRemove(line.id)}
                       disabled={loadingLineId === line.id}
                     >
-                      Remove
+                      REMOVE
                     </button>
                   </div>
                 </div>
